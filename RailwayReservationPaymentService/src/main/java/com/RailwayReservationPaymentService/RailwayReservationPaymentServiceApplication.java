60c10d4430fd751e36db5b34b1c8edbf219da23e
shweta1804 <pawarshweta1432@gmail.com>
2022-01-06 00:37:50 +0530

    Backend code committed

---
+++ RailwayReservationPaymentService/src/main/java/com/RailwayReservationPaymentService/RailwayReservationPaymentServiceApplication.java
@@ -0,0 +1,26 @@
+package com.RailwayReservationPaymentService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import it.ozimov.springboot.mail.configuration.EnableEmailTools;
+/**
+ * @author Shweta Pawar
+ * Main Method For Payment Microservice
+ */
+@SpringBootApplication
+@EnableEmailTools
+public class RailwayReservationPaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwayReservationPaymentServiceApplication.class, args);
+	}
+	
+	@Bean
+	public RestTemplate getRestTemplate()
+	{
+		return new RestTemplate();
+	} 
+}
---
+++ RailwayReservationPaymentService/src/main/java/com/RailwayReservationPaymentService/exception/ResourceNotFoundException.java
@@ -0,0 +1,17 @@
+package com.RailwayReservationPaymentService.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * @author Shweta Pawar 
+ * Custom Exception For ResourceNotFoundException
+ */
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(String message) {
+		super(message);
+	}
+}
